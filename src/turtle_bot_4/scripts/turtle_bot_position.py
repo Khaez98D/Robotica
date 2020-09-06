5d9acfa7f932f28aaf801208b4bb8d92c351d2b2
Santiago Martínez <ssantiago.mc@outlook.com>
2020-09-05 19:40:48 -0500

    Creación paquete turtle_bot_4 para el Taller 1

---
+++ src/turtle_bot_4/scripts/turtle_bot_position.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import rospy;
+import traceback;
+import math;
+from geometry_msgs.msg import Twist;
+import matplotlib.pyplot as plt;
+
+xPos = [0.0];
+yPos = [0.0];
+
+def recibido(msg):
+    print(msg.linear.x);
+    if msg.linear.x != xPos[-1] or msg.linear.y != yPos[-1]:
+        xPos.append(msg.linear.x);
+        yPos.append(msg.linear.y);
+        actGraph();
+
+def actGraph():
+    plt.subplot(111);
+    plt.clf();
+    plt.axis([-2.5, 2.5, -2.5, 2.5]);
+    plt.plot(xPos, yPos);
+    plt.scatter(xPos[-1], yPos[-1], s=40);
+    plt.pause(0.01);
+    # plt.savefig("/home/robotica/catkin_ws/src/turtle_bot_4/results/trayectoria_punto2.png");
+
+
+def turtlePos():
+    rospy.init_node('turtle_bot_position');
+    rospy.Subscriber('turtlebot_position', Twist, recibido);
+    rospy.spin();
+
+
+if __name__ == '__main__':
+    try:
+        turtlePos();
+    except Exception as e:
+        print(traceback.format_exc());
