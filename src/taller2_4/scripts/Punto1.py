f3893500d416f0d6ab309b02151d3971f5a32534
Martin <feik9810d@gmail.com>
2020-09-21 13:25:00 -0500

    Correciones punto 1, Comentarios punto 1, Movimiento punto 2, comentarios punto 2

--- src/taller2_4/scripts/Punto1.py
+++ src/taller2_4/scripts/Punto1.py
@@ -6,13 +6,16 @@ import matplotlib.pyplot as plt
 posRobot=np.array([1,0.5,np.pi/4])  #Respecto al marco inercial
 posLaser=np.array([0.2,0,0])        #Respecto al marco del robot
 
-#Carga los dato
+#Carga los datos
 pathArchivo = os.path.join(os.path.dirname(os.path.dirname(__file__)),'docs/laserscan.dat')
 pathDirectorio = os.path.join(os.path.dirname(os.path.dirname(__file__)),'results')
 laserdata = np.loadtxt(pathArchivo)
 angle_min,angle_max=-np.pi/2,np.pi/2
 theta = np.linspace(angle_min, angle_max, len(laserdata))
 
+#Función para calcular la matriz de rotación del angulo O
+#Param:o, angulo en radianes para calcular la matriz de rotacion
+#return: Matriz de rotacion ya calculada
 def R(o):
     return np.array([
         [np.cos(o),np.sin(o),0],
@@ -20,40 +23,48 @@ def R(o):
         [0,0,1],
     ])
 
+
+#Función para calcular la matriz inversa de rotación del angulo O
+#Param:o, angulo en radianes para calcular la matriz de rotacion
+#Return: Matriz de rotación ya calculada
 def Rinv(o):
     return np.array([
         [np.cos(o),-np.sin(o),0],
         [np.sin(o),np.cos(o),0],
         [0,0,1],
     ])
+    
 
 if __name__ == '__main__':
-    
     R=Rinv(posRobot[2])  #Matriz del marco del robot al marco global
     posLaserGlobal = posRobot+np.matmul(R,posLaser)
     RL= np.zeros(shape=(len(laserdata),3))   #Distancias medidas por el laser
     for i in range(len(laserdata)):
-        x=np.array([np.cos(theta[i])*laserdata[i],np.sin(theta[i])*laserdata[i],theta[i]])
-        RL[i]=(posLaserGlobal + np.matmul(R,x))
+        x=np.array([np.cos(theta[i])*laserdata[i],np.sin(theta[i])*laserdata[i],theta[i]])  #Calculo PosObjeto_Robot
+        RL[i]=(posLaserGlobal + np.matmul(R,x))  #PosObjeto_I =  PosRobot_I + R(o)*PosPobjeto_Robot
+    
+    xL=[laserdata[i]*np.cos(theta[i]) for i in range(len(laserdata))] #Componente en X de la medida del sensor
+    yL=[laserdata[i]*np.sin(theta[i]) for i in range(len(laserdata))] #Componente en Y de la medida del sensor
+    
     
     #Grafica literal A 
     plt.figure(1)
-    plt.scatter(theta,laserdata)
-    plt.xlabel(r'Angulo (rad)')
-    plt.ylabel(r'Distancia al laser (cm)')
-    plt.title(r'Mediciones del laser vs Angulo')
+    plt.scatter(xL,yL)
+    plt.xlabel(r'Posición X (m)')
+    plt.ylabel(r'Posición Y (m)')
+    plt.title(r'Mediciones del Laser desde el marco de refencia del laser')
     plt.grid()
     plt.savefig(os.path.join(pathDirectorio,'grafica1A.png'))
 
 
 
-    #Graficac literal B
+    #Graficac literal Bc
     plt.figure(2)
     plt.xlabel(r'Distancia X (m)')
     plt.ylabel(r'Distancia Y (m)')
-    plt.title(r'Grafica Robot y Laser')
-    plt.scatter(posRobot[0], posRobot[1],color='blue',marker='x')
-    plt.scatter(posLaserGlobal[0],posLaserGlobal[1],color='red', marker='x')
-    plt.scatter(RL[:,0],RL[:,1],color='black')
-    
+    plt.title(r'Mediciones del Laser desde el marco de referencia global')
+    plt.scatter(posRobot[0], posRobot[1],color='blue',marker='x',label="Posición Robot")
+    plt.scatter(posLaserGlobal[0],posLaserGlobal[1],color='red', marker='x',label="Posición Laser")
+    plt.scatter(RL[:,0],RL[:,1],color='black',label="Mediciones")
+    plt.legend()
     plt.savefig(os.path.join(pathDirectorio,'grafica1C.png'))
---
+++ src/taller2_4/scripts/Punto2.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import sys,rospy,os
+import matplotlib.pyplot as plt
+from std_msgs.msg import Float32MultiArray
+
+#Clase para el Talker de las velocidades lineales de las ruedas
+class wheelsVel():    
+    
+    #Función de inicialización
+    #Param: rutaArchivo, la ruta del archivo de texto
+    def __init__(self,rutaArchivo):
+        #Se crea e inicializa el nodo
+        self.pub = rospy.Publisher('/turtlebot_wheelsVel',Float32MultiArray,queue_size=10)
+        rospy.init_node("turtlebot_wheelsVel",anonymous=True)
+        self.rates=rospy.Rate(10)
+        
+        #Se obitienen los datos del archivo de texto
+        self.data=self.getData(rutaArchivo)
+        
+        i=0 #Contador de cuantas lineas hay que leer
+        while (not rospy.is_shutdown()) and i<len(self.data): #Ciclo, si i>len(data), significa que ya termino de leer el archivo
+            t,tv = 0,self.data[i][2]    #Contador de tiempo y tiempo durante el cual se aplica el perfil de velocidad
+            print("Se va a mandar el mensaje: ",self.data[i][0:2]) #Se notifica por consola
+            while t<tv:
+                self.pub.publish(Float32MultiArray(data=self.data[i][0:2])) #Se publica el mensaje
+                t+=1 #Sube uno el contador de tiempo
+                rospy.sleep(1)  #Se duerme durante un segundo
+                self.rates.sleep()  #Se duerme el rate al que se mandan los mensajes
+            i+=1 #Se suma 1 a la linea que se leyo
+            
+        self.pub.publish(Float32MultiArray(data=[0,0])) # Al acabar se manda este mensaje para parar el robot
+    
+    
+    #Función que extrae los datos del archivo
+    #Param: rutaArchivo, la ruta al archivo
+    #Return: data, matriz de n filas y 3 columnas, contiene los perfiles de velocidad y el tieempo de aplicacion de las n lineas
+    def getData(self,rutaArchivo):
+        with open(rutaArchivo,mode='r') as reader: #Se lee el archivo
+            lines = reader.readlines()             #Se obtienen todas las lineas
+            data=[0 for i in range(int(lines[0]))]  #Se inicializa el arreglo para N lineas
+            for i in range(1,len(lines)):
+                data[i-1] = [int(j) for j in lines[i].split()]  #Se sacan y organizan los datos
+        return data     #Retorna la estructura de datos final
+        
+#Función para verificar la ruta de archivo, si no existe enviara exepcion
+#Param: nombreArchivo, el nombre del archivo
+#return: Ruta absoluta del archivo
+#Throws: FileExistError si el archivo no existe
+def verificarRutaArchivo(nombreArchivo):
+    directorioPadre=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    ruta = os.path.join(directorioPadre,'resources/'+nombreArchivo)
+    if(os.path.isfile(ruta)):
+        return ruta
+    else:
+        raise FileExistsError
+            
+if __name__ == "__main__":
+    try:
+        if len(sys.argv)==2:   #La segunda posicion del arreglo es el nombre del archivo
+            ruta=verificarRutaArchivo(sys.argv[1])  #Se encuentra la ruta al archivo
+            publisher = wheelsVel(ruta)     #Se inicializa el nodo Publisher
+        
+        else: 
+            raise FileExistsError #Se genera error si el archivo no existe
+        
+    
+    #Excepts para manejo de los errores
+    except FileExistsError:
+        print("El nombre del archivo no es valido")
+    except rospy.ROSInterruptException:
+        print("Se interrumpio ROS")
